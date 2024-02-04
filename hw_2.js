54eaac9918f96286ab676a795f7d9d927c212cd6
Nina Malmygina <nintsya@gmail.com>
2024-02-04 13:52:35 +0100

    bug fix

--- hw_2.js
+++ hw_2.js
@@ -9,22 +9,31 @@ var schema = {
 };
 
 prompt.start();
-prompt.get(schema, function (err, result) 
-{
-if (result.age>100) console.log ("Ви щасливчик! Вам вже дуже багато, але в душі 18")
-else
-  switch (result.age % 10){
-    case 1:
-      console.log('Вам ' + result.age + ' рік');
-      break
-    case 2,3,4:
-      console.log('Вам ' + result.age + ' роки');
-      break
-    case 5,6,7,8,9,0:
-      console.log('Вам ' + result.age + ' років');
-    default:
-      console.log('На жаль, ви ввели невід\'ємне число, літери чи спецсимволи');
+prompt.get(schema, function (err, result) {
+  if (result.age>100) {
+    console.log ("Ви щасливчик! Вам вже дуже багато, але в душі 18")
   }
-}
-);
+  else {
+    switch (result.age % 10){
+      case 1:
+        console.log('Вам ' + result.age + ' рік');
+        break
+      case 2:
+      case 3:
+      case 4:
+        console.log('Вам ' + result.age + ' роки');
+        break
+      case 5:
+      case 6:
+      case 7:
+      case 8:
+      case 9:
+      case 0:
+        console.log('Вам ' + result.age + ' років');
+        break
+      default:
+        console.log('На жаль, ви ввели невід\'ємне число, літери чи спецсимволи');
+    }
+  }  
+});
 
