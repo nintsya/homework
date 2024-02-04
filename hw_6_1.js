a0fa9bf4965331062670d0fa163740d7fec726a5
Nina Malmygina <nintsya@gmail.com>
2024-02-04 21:26:06 +0100

    1

---
+++ hw_6_1.js
@@ -0,0 +1,19 @@
+
+//var s = '';
+function drawTriangle (h,s){
+    for (let i = 0; i < h; i++) {
+        for (let j = 0; j < h; j++) {
+          s=s+1
+        console.log(s);
+}
+}
+}
+
+drawTriangle(2, '*');
+
+
+// var s = "";
+// for(var i = 1; i < 11; i += 1) {
+//   s += i + " ";
+// }
+// console.log(s);
